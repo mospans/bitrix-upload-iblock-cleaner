70af27a8dbfc26686afffefc28d06627882fda3d
mospans <mospans@gmail.com>
2016-06-22 19:48:43 +0300

    Added action analysis

--- install/admin/mospans_step_cleaner.php
+++ install/admin/mospans_step_cleaner.php
@@ -1,7 +1,13 @@
 <?php
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_admin_before.php");
 
-CModule::IncludeModule("mospans.uploadiblockcleaner");
+CModule::IncludeModule('mospans.uploadiblockcleaner');
+
+if (!array_key_exists('action', $_POST)) {
+	$action = '';
+} else {
+	$action = trim($_POST['action']);
+}
 
 if (!array_key_exists('step', $_POST)) {
 	$step = -1;
@@ -9,17 +15,55 @@ if (!array_key_exists('step', $_POST)) {
 	$step = (int) $_POST['step'];
 }
 
-$result = array('error' => false);
+$result = array(
+	'error' => false,
+	'action_complete' => false
+);
 
 $stepCleaner = new CUploadIblockCleaner();
-
-if ($step == 0) {
-	$_SESSION['count_files'] = $stepCleaner->getFilesCount();
-	$_SESSION['all_steps'] = ceil($_SESSION['count_files'] / $stepCleaner->getFilesInStep());
+if (!array_key_exists('mospans.uploadiblockcleaner', $_SESSION)) {
+	$_SESSION['mospans.uploadiblockcleaner'] = array();
 }
-if ($step >= 0) {
-	
-	$result['percentage'] = round(100 * $step / $_SESSION['all_steps']);
+
+switch ($action) {
+	case 'analysis':
+		if ($step <= 0) {
+			$result['error'] = true;
+			break;
+		}
+		if (!array_key_exists('using_file_ids', $_SESSION['mospans.uploadiblockcleaner']) || $step == 1) {
+			$_SESSION['mospans.uploadiblockcleaner']['using_file_ids'] = array();
+		}
+		// получаем очередную порцию id файлов из инфоблоков:
+		$fileIds = $stepCleaner->getFilesIdInIblocksByStep($step);
+		if (is_array($fileIds)) {
+			// дописываем id файлов в сессию
+			$_SESSION['mospans.uploadiblockcleaner']['using_file_ids'] = array_merge($_SESSION['mospans.uploadiblockcleaner']['using_file_ids'], $fileIds);
+		}
+		
+		$result['percentage'] = round(100 * $step / $stepCleaner->getAnalysisSteps());
+		if ($step == $stepCleaner->getAnalysisSteps()) {
+			$result['action_complete'] = true;
+		}
+		break;
+	case 'file_processing':
+		if ($step == 0) {
+			$_SESSION['mospans.uploadiblockcleaner']['count_files'] = $stepCleaner->getFilesCount();
+			$_SESSION['mospans.uploadiblockcleaner']['all_steps'] = ceil($_SESSION['mospans.uploadiblockcleaner']['count_files'] / $stepCleaner->getFilesInStep());
+		}
+		if ($step >= 0) {
+			$files = $stepCleaner->getFilesListByStep($step);
+			foreach ($files as $file) {
+				
+			}
+			$result['percentage'] = round(100 * $step / $_SESSION['mospans.uploadiblockcleaner']['all_steps']);
+		}
+		if ($step == (int) $_SESSION['mospans.uploadiblockcleaner']['all_steps']) {
+			$result['action_complete'] = true;
+			unset($_SESSION['mospans.uploadiblockcleaner']['count_files']);
+			unset($_SESSION['mospans.uploadiblockcleaner']['all_steps']);
+		}
+		break;
 }
 echo json_encode($result);
 ?> 
--- install/js/mospans.uploadiblockcleaner/script.js
+++ install/js/mospans.uploadiblockcleaner/script.js
@@ -1,9 +1,12 @@
 window.addEventListener('load', function load(event) {
 	window.removeEventListener('load', load, false);
 	
-	var step = 0;
+	var step = 1,
+	action,
+	actions = ['analysis'];
+	
 	var makeStep = function () {
-		BX.ajax.post('/bitrix/admin/mospans_step_cleaner.php', {step: step}, callbackStepCleaner);
+		BX.ajax.post('/bitrix/admin/mospans_step_cleaner.php', {action: action, step: step}, callbackStepCleaner);
 	};
 	
 	var setPercentage = function (value) {
@@ -20,13 +23,16 @@ window.addEventListener('load', function load(event) {
 		}
 		
 		var parsedData = JSON.parse(data);
-		if (!('error' in parsedData && !parsedData.error) || !('percentage' in parsedData)) {
+		if (!('error' in parsedData && !parsedData.error) || !('percentage' in parsedData) || !('action_complete' in parsedData)) {
 			alert('Error!');
+			document.querySelectorAll('.mospans-progressbar')[0].style.display = 'none';
+			document.querySelectorAll('.mospans-run-clean')[0].style.display = 'block';
 			return;
 		}
 		
 		setPercentage(parsedData.percentage);
-		if (parsedData.percentage < 100) {
+		
+		if (!parsedData.action_complete) {
 			step++;
 			makeStep();
 		}
@@ -35,6 +41,7 @@ window.addEventListener('load', function load(event) {
 	var button = document.querySelectorAll('.mospans-run-clean')[0].addEventListener('click', function (event) {
 		event.preventDefault();
 		setPercentage(0);
+		action = actions.shift();
 		makeStep();
 	});
 }, false);
