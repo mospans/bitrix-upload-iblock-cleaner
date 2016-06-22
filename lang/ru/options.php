ed82a899d395ea841fe10eba9cacd86bbd6425ea
mospans <mospans@gmail.com>
2016-06-22 20:54:23 +0300

    Changed texts about analysis

--- lang/ru/options.php
+++ lang/ru/options.php
@@ -1,8 +1,8 @@
 <?
 $MESS['OPTIONS_TITLE'] = 'Очистка директории /upload/iblock/';
 $MESS['OPTIONS_RUN_CLEAN'] = 'Запустить очистку';
-$MESS['OPTIONS_ACTION_STATE_ANALYSIS'] = 'Анализ...';
-$MESS['OPTIONS_ACTION_STATE_ANALYSIS_COMPLETE'] = 'Анализ завершен.';
+$MESS['OPTIONS_ACTION_STATE_ANALYSIS'] = 'Идет анализ файлов в инфоблоках...';
+$MESS['OPTIONS_ACTION_STATE_ANALYSIS_COMPLETE'] = 'Анализ файлов в инфоблоках завершен.';
 $MESS['OPTIONS_ACTION_STATE_FILE_PROCESSING'] = 'Обработка файлов... Не закрывайте вкладку!';
 $MESS['OPTIONS_ACTION_STATE_FILE_PROCESSING_COMPLETE'] = 'Обработка файлов завершена';
 ?>
